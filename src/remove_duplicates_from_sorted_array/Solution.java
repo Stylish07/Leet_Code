b85ab197490b8e5e7853c86fe84c81dd4deb456b
Stylish07 <xx3satellite@gmail.com>
2022-06-02 00:03:01 +0900

    26. Remove Duplicates from Sorted Array

---
+++ src/remove_duplicates_from_sorted_array/Solution.java
@@ -0,0 +1,21 @@
+package remove_duplicates_from_sorted_array;
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int[] deduplicateNum = new int[nums.length];
+        deduplicateNum[0] = nums[0];
+        int curr = 0;
+
+        for (int num : nums) {
+            if (num != deduplicateNum[curr]) {
+                curr++;
+                deduplicateNum[curr] = num;
+            }
+        }
+        curr++;
+
+        System.arraycopy(deduplicateNum, 0, nums, 0, curr);
+
+        return curr;
+    }
+}
