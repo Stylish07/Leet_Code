d27e726bf8bda2b8ed832b256a6c2003bacab0cf
Stylish07 <xx3satellite@gmail.com>
2022-06-04 02:19:19 +0900

    27. Remove Element

---
+++ src/remove_element/Solution.java
@@ -0,0 +1,19 @@
+package remove_element;
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int[] removedNums = new int[nums.length];
+        int curr = 0;
+
+        for (int num : nums) {
+            if (num != val) {
+                removedNums[curr] = num;
+                curr++;
+            }
+        }
+
+        System.arraycopy(removedNums, 0, nums, 0, curr);
+
+        return curr;
+    }
+}
