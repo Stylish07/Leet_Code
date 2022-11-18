8598a9849265f24e58f7993c212c904dc2167733
Stylish07 <xx3satellite@gmail.com>
2022-11-18 22:35:47 +0900

    58. Length of Last Word

---
+++ src/length_of_last_word/Solution.java
@@ -0,0 +1,25 @@
+package length_of_last_word;
+
+public class Solution {
+	public int lengthOfLastWord(String s) {
+		int wordLength = 0;
+		boolean findLastWord = false;
+
+		for (int i = s.length() - 1; i >= 0; i--) {
+			String nowChar = s.substring(i, i + 1);
+			System.out.println(nowChar);
+
+			if (!nowChar.equals(" ")) {
+				wordLength++;
+				findLastWord = true;
+			}
+
+			if (findLastWord && nowChar.equals(" ")) {
+				return wordLength;
+			}
+
+		}
+
+		return wordLength;
+	}
+}
